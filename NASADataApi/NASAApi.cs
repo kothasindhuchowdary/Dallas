347845d8d4b14030915e305031b2b2693aba06fe
Sowmya <sowmyarao1217@gmail.com>
2024-04-03 09:47:51 +0530

    code changes

---
+++ NASADataApi/NASAApi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace NASADataApi
+{
+    public class NASAApi
+    {
+        const string nasaApodUrl = "https://api.nasa.gov/planetary/apod?api_key=oHneS2DMNT9l0ZyMwn6xNgngXMHBpX94dGrnU58Q&date=";
+
+        private readonly HttpClient _httpClient;
+
+        public NASAApi(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<NASADM> GetImagesByDateAsync(DateTime date)
+        {
+            var searchUrl = nasaApodUrl + date.ToString("yyyy-MM-dd");
+
+            try
+            {
+                var jsonString = await _httpClient.GetStringAsync(searchUrl);
+                return JsonConvert.DeserializeObject<NASADM>(jsonString);
+            }
+            catch (HttpRequestException ex)
+            {
+                // Handle error
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
---
+++ NASAPhotoDownloader/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NASADataApi;
+
+namespace NASAPhotoDownloader
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Start downloading...");
+            await DownloadFilesAsync();
+            Console.WriteLine("Download completed.");
+        }
+
+        static async Task DownloadFilesAsync()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var nasaapi = new NASAApi(httpClient);
+                NASADM data;
+
+                using (var fileStream = new FileStream("dates.txt", FileMode.Open))
+                using (var reader = new StreamReader(fileStream))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string line = await reader.ReadLineAsync();
+
+                        DateTime? date = DateParser.ParseDate(line);
+                        if (date != null)
+                        {
+                            data = await nasaapi.GetImagesByDateAsync((DateTime)date);
+
+                            if (!string.IsNullOrEmpty(data.url))
+                            {
+                                using (HttpClient client = new HttpClient())
+                                {
+                                    using (var response = await client.GetAsync(data.url))
+                                    {
+                                        response.EnsureSuccessStatusCode();
+
+                                        Directory.CreateDirectory("DownloadedImages");
+                                        string fileName = $"{data.date.Year}-{data.date.Month:D2}-{data.date.Day:D2}.jpg";
+                                        string filePath = Path.Combine("DownloadedImages", fileName);
+
+                                        using (var fileStreamInner = new FileStream(filePath, FileMode.Create))
+                                        {
+                                            await response.Content.CopyToAsync(fileStreamInner);
+                                        }
+
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
