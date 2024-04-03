8946366403e1aa12ded05153019549e7a1403758
Sowmyavar <sowmyarao1217@gmail.com>
2024-04-03 12:51:01 -0500

    UI & API changes

--- NASAPicturesAPI/Program.cs
+++ NASAPicturesAPI/Program.cs
@@ -1,18 +1,26 @@
 using NASADataApi;
 
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 // Add services to the container.
-
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy(name: MyAllowSpecificOrigins,
+                    policy =>
+                    {
+                      policy.AllowAnyOrigin();// WithOrigins("http://localhost:4200/")
+                    });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-var allowedorigins = "_allowedorigins";
-builder.Services.AddCors(options =>
-{
-  options.AddPolicy(name: allowedorigins, policy => { policy.WithOrigins(allowedorigins); });
-});
+
+
+
+
+
 builder.Services.AddHttpClient<INASAApi, NASAApi>(client =>
 {
   client.BaseAddress = new Uri("https://api.nasa.gov/");
@@ -29,7 +37,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseCors(MyAllowSpecificOrigins);
 
 app.MapControllers();
-
 app.Run();
